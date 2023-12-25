20249d9684022ff8ce2d3e309b8d682aa7a196c6
One-armed-boy <fanatic5500@gmail.com>
2023-12-25 16:53:46 +0900

    Write File Upload Basement Logic without Real Upload

--- src/main/java/com/stream/video/service/LocalVideoServiceImpl.java
+++ src/main/java/com/stream/video/service/LocalVideoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.stream.video.service;
 
 import com.stream.video.domain.Video;
+import com.stream.video.dto.UploadVideoDto;
 import com.stream.video.dto.VideoDto;
 import com.stream.video.repository.VideoRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -9,50 +10,61 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @Qualifier("LocalVideoService")
 public class LocalVideoServiceImpl implements VideoService {
-    @Autowired
-    private VideoRepository videoRepository;
+  @Autowired
+  private VideoRepository videoRepository;
 
-    @Value("${video.dir}")
-    String videoDir;
+  @Value("${video.dir}")
+  String videoDir;
 
-    @Override
-    public List<VideoDto> listVideo() {
-        List<Video> videoList = videoRepository.findAll();
-        return videoList.stream()
-                .map(video -> convertDomainToDto(video))
-                .toList();
-    }
+  @Override
+  public List<VideoDto> listVideo() {
+    List<Video> videoList = videoRepository.findAll();
+    return videoList.stream()
+            .map(video -> convertDomainToDto(video))
+            .toList();
+  }
 
-    @Override
-    public VideoDto getVideoMetadata(long id) throws EntityNotFoundException {
-        Video video = videoRepository.getReferenceById(id);
-        return convertDomainToDto(video);
-    }
+  @Override
+  public VideoDto getVideoMetadata(long id) throws EntityNotFoundException {
+    Video video = videoRepository.getReferenceById(id);
+    return convertDomainToDto(video);
+  }
 
-    @Override
-    public Resource createVideoStream(long id) throws FileNotFoundException {
-        Video video = videoRepository.getReferenceById(id);
-        Path videoPath = Paths.get(video.getPath());
-        return new InputStreamResource(new FileInputStream(videoPath.toString()));
-    }
+  @Override
+  @Async
+  public CompletableFuture uploadVideoAsync(UploadVideoDto videoMetadata, MultipartFile videoFile) {
+    // TODO: 비동기 파일 업로드 로직 구현
+    return CompletableFuture.completedFuture(null);
+  }
 
-    private VideoDto convertDomainToDto(Video video) {
-        return VideoDto.builder()
-                .id(video.getId())
-                .fileName(video.getFileName())
-                .size(video.getSize())
-                .description(video.getDescription())
-                .createdAt(video.getCreatedAt())
-                .build();
-    }
+  @Override
+  public Resource createVideoStream(long id) throws FileNotFoundException {
+    Video video = videoRepository.getReferenceById(id);
+    Path videoPath = Paths.get(video.getPath());
+    return new InputStreamResource(new FileInputStream(videoPath.toString()));
+  }
+
+  private VideoDto convertDomainToDto(Video video) {
+    return VideoDto.builder()
+            .id(video.getId())
+            .fileName(video.getFileName())
+            .size(video.getSize())
+            .description(video.getDescription())
+            .createdAt(video.getCreatedAt())
+            .build();
+  }
 }
--- src/main/java/com/stream/video/service/VideoService.java
+++ src/main/java/com/stream/video/service/VideoService.java
@@ -1,14 +1,21 @@
 package com.stream.video.service;
 
+import com.stream.video.dto.UploadVideoDto;
 import com.stream.video.dto.VideoDto;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public interface VideoService {
-    List<VideoDto> listVideo();
-    VideoDto getVideoMetadata(long id) throws EntityNotFoundException;
-    Resource createVideoStream(long id) throws FileNotFoundException;
+  List<VideoDto> listVideo();
+
+  VideoDto getVideoMetadata(long id) throws EntityNotFoundException;
+
+  CompletableFuture uploadVideoAsync(UploadVideoDto videoMetadata, MultipartFile videoFile);
+
+  Resource createVideoStream(long id) throws FileNotFoundException;
 }
