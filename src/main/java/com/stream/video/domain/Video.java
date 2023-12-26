c9782b0c1bac4e73eb2da21700637dc7e3850105
One-armed-boy <fanatic5500@gmail.com>
2023-12-26 18:06:17 +0900

    Change Member Variable Name in Video Domain (fileName -> fileTag)

--- src/main/java/com/stream/video/domain/Video.java
+++ src/main/java/com/stream/video/domain/Video.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -13,41 +14,41 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 public class Video {
-    @Id
-    @Column(name = "id", nullable = false, updatable = false)
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+  @Id
+  @Column(name = "id", nullable = false, updatable = false)
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
 
-    @Column(name = "file_name", nullable = false)
-    private String fileName;
+  @Column(name = "file_tag", nullable = false)
+  private String fileTag;
 
-    @Column(name = "extension", nullable = false)
-    private String extension;
+  @Column(name = "extension", nullable = false)
+  private String extension;
 
-    @Column(name = "path", nullable = false)
-    private String path;
+  @Column(name = "path", nullable = false)
+  private String path;
 
-    @Column(name = "size", nullable = false)
-    private long size;
+  @Column(name = "size", nullable = false)
+  private long size;
 
-    @Column(name = "description")
-    private String description;
+  @Column(name = "description")
+  private String description;
 
-    @CreationTimestamp
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
 
-    @UpdateTimestamp
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+  @UpdateTimestamp
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
 
-    @Builder
-    public Video(long id, String fileName, String extension, String path, long size, String description) {
-        this.id = id;
-        this.fileName = fileName;
-        this.extension = extension;
-        this.path = path;
-        this.size = size;
-        this.description = description;
-    }
+  @Builder
+  public Video(long id, String fileTag, String extension, String path, long size, String description) {
+    this.id = id;
+    this.fileTag = fileTag;
+    this.extension = extension;
+    this.path = path;
+    this.size = size;
+    this.description = description;
+  }
 }
