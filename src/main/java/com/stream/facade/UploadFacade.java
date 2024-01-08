c7d1285f6ec9177458d30cbfa0c0f28843d9e29b
One-armed-boy <fanatic5500@gmail.com>
2024-01-08 22:55:17 +0900

    Delete unnecessary method in UploadFacade

--- src/main/java/com/stream/facade/UploadFacade.java
+++ src/main/java/com/stream/facade/UploadFacade.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -38,29 +36,6 @@ public class UploadFacade {
     } finally {
       return CompletableFuture.completedFuture(null);
     }
-  }
-
-  private void checkTargetDir(File targetDir) {
-    if (targetDir.exists()) {
-      return;
-    }
-    targetDir.mkdir();
-  }
-
-  private void saveFile(File targetFile, MultipartFile videoData) throws IOException {
-    InputStream inputStream = videoData.getInputStream();
-    OutputStream outputStream = new FileOutputStream(targetFile);
-
-    byte[] buffer = new byte[1024];
-    int bytesRead;
-
-    while ((bytesRead = inputStream.read(buffer)) != -1) {
-      outputStream.write(buffer, 0, bytesRead);
-    }
-  }
-
-  private String createRandomName(String extension) {
-    return UUID.randomUUID() + "." + extension;
   }
 
   private void execPostSave(UploadVideoDto videoMetadata, String targetPath, long fileSize) {
