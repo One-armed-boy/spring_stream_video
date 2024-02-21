e6ab7e7a89590e12bde2000b07539b7e234555a1
One-armed-boy <fanatic5500@gmail.com>
2024-02-21 13:33:33 +0900

    구성 클래스 명 수정 및 SpringSecurity 구성을 위한 초안 작성 (CommonConfig -> SecurityConfig)

---
+++ src/main/java/com/stream/configuration/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.stream.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.build();
+	}
+}
