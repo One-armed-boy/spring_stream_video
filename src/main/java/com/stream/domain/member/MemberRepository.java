105face67a1261341d5a42ba417d64b079aeec8a
One-armed-boy <fanatic5500@gmail.com>
2024-02-21 13:46:19 +0900

    불필요한 코드 조각 삭제 처리

--- src/main/java/com/stream/domain/member/MemberRepository.java
+++ src/main/java/com/stream/domain/member/MemberRepository.java
@@ -5,5 +5,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-	public Optional<Member> findMemberByEmail(String email);
+	Optional<Member> findMemberByEmail(String email);
 }
