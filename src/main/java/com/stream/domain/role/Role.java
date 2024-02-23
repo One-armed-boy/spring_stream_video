89aacf78eaf93166f5f2e5b6da6dad4da67dc8df
One-armed-boy <fanatic5500@gmail.com>
2024-02-24 00:00:13 +0900

    누락된 수정점 추가 (Role -> 기본 생성자 노출 범위 조정)

--- src/main/java/com/stream/domain/role/Role.java
+++ src/main/java/com/stream/domain/role/Role.java
@@ -8,12 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "roles")
-@NoArgsConstructor
 @Getter
 public class Role {
 	@Id
